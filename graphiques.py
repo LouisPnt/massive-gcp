cc88bd48d80cee84f22a9c3bf692aba99f778e7f
LouisPnt <louispenet2004@gmail.com>
2025-11-24 23:31:27 +0100

    ajout graphes et fichiers csv

---
+++ graphiques.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv("conc.csv")  # Remplace "data.csv" par le nom de ton fichier
+#data = pd.read_csv("post.csv")
+#data = pd.read_csv("fanout.csv")
+
+
+stats = data.groupby('PARAM')['AVG_TIME'].agg(['mean', 'std']).reset_index()
+
+plt.figure(figsize=(8,5))
+plt.bar(stats['PARAM'].astype(str), stats['mean'], yerr=stats['std'], capsize=5, color='cornflowerblue')
+plt.xlabel("Nombre d'utilisateurs concurrents")
+#plt.xlabel("Nombre de post par user")
+#plt.xlabel("Nombre de followee")
+plt.ylabel("Temps moyen par requête (s)")
+#plt.title("Temps moyen par requête selon le nombre de posts")
+#plt.title("Temps moyen par requête selon le nombre de followee")
+plt.title("Temps moyen par requête timeline")
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
