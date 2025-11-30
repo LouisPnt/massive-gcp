432f50735586a68d4cd4c072f164aba74d1969f8
LouisPnt <louispenet2004@gmail.com>
2025-11-30 19:30:08 +0100

    maj graphs + csv

--- graphiques.py
+++ graphiques.py
@@ -2,9 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv("conc.csv")  # Remplace "data.csv" par le nom de ton fichier
-#data = pd.read_csv("post.csv")
-#data = pd.read_csv("fanout.csv")
+#data = pd.read_csv("out/conc.csv")  # Remplace "data.csv" par le nom de ton fichier
+#data = pd.read_csv("out/post.csv")
+data = pd.read_csv("out/fanout.csv")
 
 
 stats = data.groupby('PARAM')['AVG_TIME'].agg(['mean', 'std']).reset_index()
@@ -14,9 +14,9 @@ plt.bar(stats['PARAM'].astype(str), stats['mean'], yerr=stats['std'], capsize=5,
 plt.xlabel("Nombre d'utilisateurs concurrents")
 #plt.xlabel("Nombre de post par user")
 #plt.xlabel("Nombre de followee")
-plt.ylabel("Temps moyen par requête (s)")
+#plt.ylabel("Temps moyen par requête (s)")
 #plt.title("Temps moyen par requête selon le nombre de posts")
-#plt.title("Temps moyen par requête selon le nombre de followee")
-plt.title("Temps moyen par requête timeline")
+plt.title("Temps moyen par requête selon le nombre de followee")
+#plt.title("Temps moyen par requête timeline")
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
